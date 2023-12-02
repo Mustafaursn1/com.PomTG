5671d9e0b8878ff7e0c8fe2f85d261c4f56b34ca
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-12-02 21:19:34 +0100

    add ConfigurationReaderPractice

--- src/test/java/test/day18/C02_ConfigurationProperties.java
+++ src/test/java/test/day18/C02_ConfigurationProperties.java
@@ -1,11 +1,16 @@
 package test.day18;
 
 import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class C02_ConfigurationProperties {
     @Test
-    public void test01(){
-        Driver.getDriver().get("amazonUrl");
+    public void test01() {
+        String urlAmz = ConfigurationReader.getProperty("amazonUrl");
+        String urlFace = ConfigurationReader.getProperty("faceUrl");
+        Driver.getDriver().get(urlAmz);
+        Driver.getDriver().get(urlFace);
+        Driver.quitDriver();
     }
 }
